b757c2deb1a38da2264f45e5ac2059e47319890c
ramyaperam <peram.ramya77@gmail.com>
2023-06-19 15:09:29 -0500

    Static example

---
+++ src/corejava5/StaticDemo.java
@@ -0,0 +1,10 @@
+package corejava5;
+
+public class StaticDemo {
+    static int num1=100;
+    static int num2=200;
+    static void displayNum1()
+    {
+        System.out.println("num1 :" +num1 );
+    }
+}
---
+++ src/corejava5/StaticDemoMain.java
@@ -0,0 +1,9 @@
+package corejava5;
+
+public class StaticDemoMain {
+    public static void main(String [] args)
+    {
+        StaticDemo.displayNum1();
+        System.out.println("Num2 :" +StaticDemo.num2);
+    }
+}
