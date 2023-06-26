034b2ab0614bdcd3beb24b646a465516155d2bc3
ramyaperam <peram.ramya77@gmail.com>
2023-06-26 14:41:38 -0500

    Throw demo

---
+++ src/corejava10/ThrowsDemo.java
@@ -0,0 +1,7 @@
+package corejava10;
+
+public class ThrowsDemo {
+    static void m1 () throws ArithmeticException{
+        System.out.println(10/0);
+    }
+}
---
+++ src/corejava10/ThrowsDemoMain.java
@@ -0,0 +1,13 @@
+package corejava10;
+
+public class ThrowsDemoMain {
+    public static void main (String [] args )
+    {
+        try {
+            ThrowsDemo.m1();
+        }catch(ArithmeticException e)
+        {
+
+        }
+    }
+}
